141d639363d1d426026c48d76bfa18454d43315b
huscse <husnainkhaliq82@gmail.com>
2025-10-16 21:14:19 -0400

    remove footer border

--- frontend/app/components/Footer.jsx
+++ frontend/app/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div>
       {/* Footer */}
-      <footer className="border-t border-slate-800 py-8">
+      <footer className="py-8">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center text-slate-400">
             <p>&copy; 2025 Navis. All rights reserved.</p>
